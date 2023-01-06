787ed9d3a4cb276a3f532c694ffe48401fe5e0c8
dyyllaann <dylan.arveson@gmail.com>
2023-01-05 20:51:04 -0800

    Add date input elements.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import modeButton from './mode-button.png';
 import './App.css';
+import moment from "moment";
+
+/* MUI IMPORTS */
+// import Box from "@mui/material/Box";
+import TextField from '@mui/material/TextField';
+// import Button from '@mui/material/Button';
+// import DeleteIcon from "@mui/icons-material/Delete";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<ThemeProvider theme={darkTheme}>
+			<div className="App">
+				<header className="App-header">
+					<img src={modeButton} className="logoButton" alt="Change mode." />
+				</header>
+				<div className="calendarInput">
+					<h1>Enter start date</h1>
+					<LocalizationProvider dateAdapter={AdapterMoment}>
+						<DatePicker
+							className="datePicker"
+							label="Date"
+							value={null}
+							renderInput={(params) => <TextField size="small" {...params} />}
+						/>
+					</LocalizationProvider>
+				</div>
+				<div className="calendarInput">
+					<h1>Enter end date</h1>
+					<LocalizationProvider dateAdapter={AdapterMoment}>
+						<DatePicker
+							className="datePicker"
+							label="Date"
+							value={null}
+							renderInput={(params) => <TextField size="small" {...params} />}
+						/>
+					</LocalizationProvider>
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
