ba449427209356196cbf2db23e503f53cbde7e81
dyyllaann <dylan.arveson@gmail.com>
2023-01-08 14:08:18 -0800

    Add each options element.

--- src/ToDate.js
+++ src/ToDate.js
@@ -6,13 +6,13 @@ import moment from "moment";
 /* MUI IMPORTS */
 // import Box from "@mui/material/Box";
 import TextField from '@mui/material/TextField';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 // import DeleteIcon from "@mui/icons-material/Delete";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Typography } from "@mui/material";
+import { Switch, Typography } from "@mui/material";
 
 const darkTheme = createTheme({
 	palette: {
@@ -53,6 +53,25 @@ function ToDate() {
             // }}
             // variant="outlined"
           />
+        </div>
+      </div>
+      <div className="optionsContainer">
+        <div className="optionsWeekend">
+          <Switch />
+          <Typography
+            color="text.primary"
+          >Skip weekends</Typography>
+        </div>
+        <div className="optionsHoliday">
+          <Switch />
+          <Typography
+            color="text.primary"
+            justifyContent="center"
+          >Skip holidays</Typography>
+        </div>
+        <div className="optionsActions">
+          <Button className="optionsButton">Settings</Button>
+          <Button className="optionsButton">OK</Button>
         </div>
       </div>
     </section>
